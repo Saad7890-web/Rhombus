bc1cb4566f8debc5aa1569907e30ff6e7337a586
Saad7890-web <omysaadislam@gmail.com>
2026-05-27 22:50:57 +0600

    added kafka config

---
+++ internal/delivery/kafka/config.go
@@ -0,0 +1,22 @@
+package kafka
+
+import "time"
+
+type Config struct {
+	Brokers         []string
+	ClientID        string
+	TopicPrefix     string
+	RequiredAcks    int
+	BatchTimeout    time.Duration
+	Async           bool
+	AllowAutoCreate bool
+}
+
+func DefaultConfig() Config {
+	return Config{
+		RequiredAcks:    -1, // all replicas
+		BatchTimeout:    10 * time.Millisecond,
+		Async:           false,
+		AllowAutoCreate: false,
+	}
+}
